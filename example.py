419db2e9c523c76c940f742190bb590002b7b3d6
sandyaspen <sandyaspenwood@gmail.com>
2021-03-02 18:18:36 -0800

    Adding simple example file from openai gym for env testing

---
+++ example.py
@@ -0,0 +1,9 @@
+import gym
+
+#Barebones example from https://gym.openai.com/docs/#Environments
+env = gym.make('CartPole-v0')
+env.reset()
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample()) # take a random action
+env.close()
