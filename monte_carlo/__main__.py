1088e36e44406d745f9df43985ded4f66badcdea
pniyongabo <patrickniyongabo@gmail.com>
2021-03-06 20:44:17 +0200

    Added a template file for Monte Carlo methods

--- monte_carlo/__main__.py
+++ monte_carlo/__main__.py
@@ -12,14 +12,14 @@ DISCOUNT = 0.9
 EPSILON = 1
 EPSILON_DECAY = 0.97
 
-
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "load":
-        if path.exists("td_q_matrix.pickle"):
-            with open("td_q_matrix.pickle", "rb") as f:
+        if path.exists("mc_q_matrix.pickle"):
+            with open("mc_q_matrix.pickle", "rb") as f:
                 q = pickle.load(f)
     else:
-        q = np.zeros((14,15,3), dtype=float)
+        q = np.zeros((18,140,3), dtype=float)
+        # q table (columns: car position (-1.2 to 0.6), car velocity (-0.07 to 0.07), action (0,1,2))
 
     env = gym.make('MountainCar-v0')
     total_episode_rewards = []
@@ -35,18 +35,22 @@ if __name__ == "__main__":
                 action = env.action_space.sample()
             else:
                 action = np.argmax(q[state])
-            #PERFORM ACTION
-            #COLLECT NEW STATE, REWARD, and DONE
+            #PERFORM ACTION  
+            # COLLECT NEW STATE, REWARD, and DONE
+            observation, reward, done, info = env.step(action)
             #ADJUST AND ROUND NEW STATE
-            #UPDATE q
+            observation = (round(observation[0] - env.observation_space.low[0], 1), round(observation[1] - env.observation_space.low[1], 2))
+            #UPDATE q ???
             #ADD REWARD TO EPISODE_REWARDS
+            episode_rewards += reward
             #Lower epsilon some amount or check to lower epsilon if episode % SOME_VALUE == 0
         
         #ADD EPISODE REWARD TO TOTAL_EPISODE_REWARDS
+        total_episode_rewards.append(episode_rewards)
 
 
     #Save the trained q matrix 
-    with open("td_q_matrix.pickle", "wb") as f:
+    with open("mc_q_matrix.pickle", "wb") as f:
         pickle.dump(q, f)
 
 
