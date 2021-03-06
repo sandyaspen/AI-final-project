b6b6ae9f72eb9c532713a8d61fca6b9c8c38a728
pniyongabo <patrickniyongabo@gmail.com>
2021-03-06 21:25:07 +0200

    updated mc template and added pickle files to gitignore

--- monte_carlo/__main__.py
+++ monte_carlo/__main__.py
@@ -18,15 +18,17 @@ if __name__ == "__main__":
             with open("mc_q_matrix.pickle", "rb") as f:
                 q = pickle.load(f)
     else:
-        q = np.zeros((18,140,3), dtype=float)
+        q = np.zeros((18,15,3), dtype=float)
         # q table (columns: car position (-1.2 to 0.6), car velocity (-0.07 to 0.07), action (0,1,2))
 
     env = gym.make('MountainCar-v0')
     total_episode_rewards = []
     epsilon = EPSILON
+    alpha = LEARNING_RATE
+    gamma = DISCOUNT
     for episode in range(EPISODES):
         state = env.reset()
-        state = (round(state[0] - env.observation_space.low[0], 1), round(state[1] - env.observation_space.low[1], 2))
+        state = (int(round(state[0] - env.observation_space.low[0], 1) * 10), int(round(state[1] - env.observation_space.low[1], 2) * 100))
         done = False
         episode_rewards = 0
 
@@ -36,17 +38,31 @@ if __name__ == "__main__":
             else:
                 action = np.argmax(q[state])
             #PERFORM ACTION  
-            # COLLECT NEW STATE, REWARD, and DONE
-            observation, reward, done, info = env.step(action)
+            #COLLECT NEW STATE, REWARD, and DONE
+            next_state, reward, done, info = env.step(action)
+            
             #ADJUST AND ROUND NEW STATE
-            observation = (round(observation[0] - env.observation_space.low[0], 1), round(observation[1] - env.observation_space.low[1], 2))
-            #UPDATE q ???
+            next_state = (int(round(next_state[0] - env.observation_space.low[0], 1) * 10), int(round(next_state[1] - env.observation_space.low[1], 2) * 100))
+            
+            #UPDATE
+            Gt = reward # todo: calculate the discounted reward using gamma
+            if not done:
+                q[state][action] = q[state][action] + alpha * (Gt - q[state][action])
+            else:
+                q[state][action] = q[state][action] + alpha * (reward - q[state][action])
+            
             #ADD REWARD TO EPISODE_REWARDS
             episode_rewards += reward
-            #Lower epsilon some amount or check to lower epsilon if episode % SOME_VALUE == 0
+            
+            #set state to next_state
+            state = next_state
+        
+        #Lower epsilon some amount
+        epsilon *= EPSILON_DECAY
         
         #ADD EPISODE REWARD TO TOTAL_EPISODE_REWARDS
         total_episode_rewards.append(episode_rewards)
+        print("EPISODE: {}    REWARD: {}    EPSILON: {}".format(episode, episode_rewards, epsilon))
 
 
     #Save the trained q matrix 
@@ -55,3 +71,4 @@ if __name__ == "__main__":
 
 
     #Plot results with matplot lib
+    print(total_episode_rewards)
