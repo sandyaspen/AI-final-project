bfb4d5ab8ec7895644720393fae9c5bb25b4eb1e
Silas <silas@dmarvin.net>
2021-03-06 09:52:39 -0800

    Working temporal difference q learning

--- temporal_difference/__main__.py
+++ temporal_difference/__main__.py
@@ -6,27 +6,27 @@ import pickle
 from os import path
 
 
-EPISODES = 500
+EPISODES = 5000
 LEARNING_RATE = 0.2
-DISCOUNT = 0.9
+DISCOUNT = 1 
 EPSILON = 1
-EPSILON_DECAY = 0.97
+EPSILON_DECAY = 0.999
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "load":
         if path.exists("td_q_matrix.pickle"):
-            with open("td_q_matrix.pickel", "rb") as f:
+            with open("td_q_matrix.pickle", "rb") as f:
                 q = pickle.load(f)
     else:
-        q = np.zeros((14,15,3), dtype=float)
+        q = np.zeros((18,15,3), dtype=float)
 
     env = gym.make('MountainCar-v0')
     total_episode_rewards = []
     epsilon = EPSILON
     for episode in range(EPISODES):
         state = env.reset()
-        state = (round(state[0] - env.observation_space.low[0], 1), round(state[1] - env.observation_space.low[1], 2))
+        state = (int(round(state[0] - env.observation_space.low[0], 1) * 10), int(round(state[1] - env.observation_space.low[1], 2) * 100))
         done = False
         episode_rewards = 0
 
@@ -37,12 +37,29 @@ if __name__ == "__main__":
                 action = np.argmax(q[state])
             #PERFORM ACTION
             #COLLECT NEW STATE, REWARD, and DONE
+            next_state, reward, done, info = env.step(action)
+
             #ADJUST AND ROUND NEW STATE
+            next_state = (int(round(next_state[0] - env.observation_space.low[0], 1) * 10), int(round(next_state[1] - env.observation_space.low[1], 2) * 100))
+
             #UPDATE q
+            if not done:
+                q[state][action] = q[state][action] + LEARNING_RATE * (reward + DISCOUNT * np.amax(q[next_state]) - q[state][action])
+            else:
+                q[state][action] = q[state][action] + LEARNING_RATE * (reward - q[state][action])
+
             #ADD REWARD TO EPISODE_REWARDS
-            #Lower epsilon some amount or check to lower epsilon if episode % SOME_VALUE == 0
+            episode_rewards += reward
+
+            #Set state = next_state
+            state = next_state
+
+        #Lower epsilon some amount or check to lower epsilon if episode % SOME_VALUE == 0
+        epsilon *= EPSILON_DECAY
         
         #ADD EPISODE REWARD TO TOTAL_EPISODE_REWARDS
+        total_episode_rewards.append(episode_rewards)
+        print("EPISODE: {}    REWARD: {}    EPSILON: {}".format(episode, episode_rewards, epsilon))
 
 
     #Save the trained q matrix 
@@ -51,3 +68,4 @@ if __name__ == "__main__":
 
 
     #Plot results with matplot lib
+    print(total_episode_rewards)
