5594f970359542611bfffaeb994225658c55710b
sandyaspen <sandyaspenwood@gmail.com>
2021-03-15 00:21:47 -0700

    move temporal difference and monte carlo functions to classes

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,81 +1,95 @@
 import numpy as np
 import random
 import gym
-import sys
-import math
 
+import pandas as pd
 
 EPISODES = 5000
-LEARNING_RATE = 0.2
 DISCOUNT = .95
 EPSILON = 1
 EPSILON_DECAY = 0.997
-import pandas as pd
 
+class Monte_Carlo:
+    def __init__(self):
+        self.env = gym.make('CartPole-v1')
+        self.cart_position_bins = pd.cut([self.env.observation_space.low[0], self.env.observation_space.high[0]], bins=5, retbins=True)[1]
+        self.cart_velocity_bins = pd.cut([self.env.observation_space.low[1], self.env.observation_space.high[1]], bins=5, retbins=True)[1]
+        self.pole_angle_bins = pd.cut([self.env.observation_space.low[2], self.env.observation_space.high[2]], bins=10, retbins=True)[1]
+        self.pole_angular_velocity_bins = pd.cut([self.env.observation_space.low[3], self.env.observation_space.high[3]], bins=10, retbins=True)[1]
+        self.episodes=EPISODES
+        self.discount=DISCOUNT 
+        self.epsilon=EPSILON
+        self.epsilon_decay=EPSILON_DECAY
 
-env = gym.make('CartPole-v1')
+    def bin(self, val, bins):
+        for i in range(len(bins)):
+            if val >= bins[i] and val < bins[i+1]:
+                return i
+    
+    def set_hyper_params(self, **kwargs):
+        if 'episodes' in kwargs:
+            self.episodes = kwargs['episodes']
+        if 'discount' in kwargs:
+            self.discount = kwargs['discount']
+        if 'epsilon' in kwargs:
+            self.epsilon = kwargs['epsilon']
+        if 'epsilon_decay' in kwargs:
+            self.epsilon_decay = kwargs['epsilon_decay']
+        
+    def discretize(self, state):
+        return (self.bin(state[0], self.cart_position_bins), 
+                self.bin(state[1], self.cart_velocity_bins), 
+                self.bin(state[2], self.pole_angle_bins), 
+                self.bin(state[3], self.pole_angular_velocity_bins))
 
+    def run(self):
+        q = np.zeros((5,5,10,10,2), dtype=float)
+        qq = np.zeros((5,5,10,10,2,2), dtype=float)
+        for episode in range(self.episodes):
+            state = self.env.reset()
+            state = self.discretize(state)
+            done = False
+            episode_rewards = 0
+            
+            episode_memory = [] # stores the (state, action, reward) tuple
 
-cart_position_bins = pd.cut([env.observation_space.low[0], env.observation_space.high[0]], bins=5, retbins=True)[1]
-cart_velocity_bins = pd.cut([env.observation_space.low[1], env.observation_space.high[1]], bins=5, retbins=True)[1]
-pole_angle_bins = pd.cut([env.observation_space.low[2], env.observation_space.high[2]], bins=10, retbins=True)[1]
-pole_angular_velocity_bins = pd.cut([env.observation_space.low[3], env.observation_space.high[3]], bins=10, retbins=True)[1]
+            while not done:
+                if random.random() < self.epsilon:
+                    action = self.env.action_space.sample()
+                else:
+                    action = np.argmax(q[state])
 
-
-def bin(val, bins):
-    for i in range(len(bins)):
-        if val >= bins[i] and val < bins[i+1]:
-            return i
-
-def discretize(state):
-    return (bin(state[0], cart_position_bins), bin(state[1], cart_velocity_bins), bin(state[2], pole_angle_bins), bin(state[3], pole_angular_velocity_bins))
+                #PERFORM ACTION & COLLECT NEW STATE, REWARD, and DONE
+                next_state, reward, done, info = self.env.step(action)
+                
+                #ADJUST AND ROUND NEW STATE
+                next_state = self.discretize(next_state)
+                
+                #UPDATE
+                episode_memory.append((state, action, reward))
+                
+                #ADD REWARD TO EPISODE_REWARDS
+                episode_rewards += reward
+                
+                #set state to next_state
+                state = next_state
+            
+            
+            # calculate discounted_reward based by iterating backward through `episode_memory_sar`
+            discounted_reward = 0
+            for state, action, reward in episode_memory[::-1]:
+                discounted_reward = reward + (self.discount * discounted_reward)      
+                qq[state][action][0] += 1
+                qq[state][action][1] += discounted_reward
+                q[state][action] = qq[state][action][1]/qq[state][action][0]
+                #q[state][action] = q[state][action] + (LEARNING_RATE * (discounted_reward - q[state][action]))
+                        
+            #Decay Epsilon
+            self.epsilon = max(self.epsilon * self.epsilon_decay, 0.1)
+            
+            #ADD EPISODE REWARD TO TOTAL_EPISODE_REWARDS
+            print("EPISODE: {}    REWARD: {}    EPSILON: {}".format(episode, episode_rewards, self.epsilon), end='\r')
 
 
 if __name__ == "__main__":
-    q = np.zeros((5,5,10,10,2), dtype=float)
-    qq = np.zeros((5,5,10,10,2,2), dtype=float)
-    epsilon = EPSILON
-    for episode in range(EPISODES):
-        state = env.reset()
-        state = discretize(state)
-        done = False
-        episode_rewards = 0
-        
-        episode_memory = [] # stores the (state, action, reward) tuple
-
-        while not done:
-            if random.random() < epsilon:
-                action = env.action_space.sample()
-            else:
-                action = np.argmax(q[state])
-
-            #PERFORM ACTION & COLLECT NEW STATE, REWARD, and DONE
-            next_state, reward, done, info = env.step(action)
-            
-            #ADJUST AND ROUND NEW STATE
-            next_state = discretize(next_state)
-            
-            #UPDATE
-            episode_memory.append((state, action, reward))
-            
-            #ADD REWARD TO EPISODE_REWARDS
-            episode_rewards += reward
-            
-            #set state to next_state
-            state = next_state
-        
-        
-        # calculate discounted_reward based by iterating backward through `episode_memory_sar`
-        discounted_reward = 0
-        for state, action, reward in episode_memory[::-1]:
-            discounted_reward = reward + (DISCOUNT * discounted_reward)      
-            qq[state][action][0] += 1
-            qq[state][action][1] += discounted_reward
-            q[state][action] = qq[state][action][1]/qq[state][action][0]
-            #q[state][action] = q[state][action] + (LEARNING_RATE * (discounted_reward - q[state][action]))
-                    
-        #Decay Epsilon
-        epsilon = max(epsilon * EPSILON_DECAY, 0.1)
-        
-        #ADD EPISODE REWARD TO TOTAL_EPISODE_REWARDS
-        print("EPISODE: {}    REWARD: {}    EPSILON: {}".format(episode, episode_rewards, epsilon))
+    Monte_Carlo().run()
--- temporal_difference.py
+++ temporal_difference.py
@@ -6,68 +6,84 @@ import pickle
 import pandas as pd
 from os import path
 
-
 EPISODES = 5000
 LEARNING_RATE = 0.2
 DISCOUNT = .95
 EPSILON = 1
 EPSILON_DECAY = 0.997
 
+class Temporal_Difference:
+    def __init__(self):
+        self.env = gym.make('CartPole-v1')
+        self.cart_position_bins = pd.cut([self.env.observation_space.low[0], self.env.observation_space.high[0]], bins=5, retbins=True)[1]
+        self.cart_velocity_bins = pd.cut([self.env.observation_space.low[1], self.env.observation_space.high[1]], bins=5, retbins=True)[1]
+        self.pole_angle_bins = pd.cut([self.env.observation_space.low[2], self.env.observation_space.high[2]], bins=10, retbins=True)[1]
+        self.pole_angular_velocity_bins = pd.cut([self.env.observation_space.low[3], self.env.observation_space.high[3]], bins=10, retbins=True)[1]
+        self.episodes=EPISODES
+        self.discount=DISCOUNT 
+        self.learning_rate=LEARNING_RATE
+        self.epsilon=EPSILON 
+        self.epsilon_decay=EPSILON_DECAY
 
-env = gym.make('CartPole-v1')
+    def set_hyper_params(self, **kwargs):
+        if 'episodes' in kwargs:
+            self.episodes = kwargs['episodes']
+        if 'discount' in kwargs:
+            self.discount = kwargs['discount']
+        if 'epsilon' in kwargs:
+            self.epsilon = kwargs['epsilon']
+        if 'epsilon_decay' in kwargs:
+            self.epsilon_decay = kwargs['epsilon_decay']
+        if 'learning_rate' in kwargs:
+            self.learning_rate = kwargs['learning_rate']
+
+    def bin(self, val, bins):
+        for i in range(len(bins)):
+            if val >= bins[i] and val < bins[i+1]:
+                return i
+
+    def discretize(self, state):
+        return (self.bin(state[0], self.cart_position_bins), self.bin(state[1], self.cart_velocity_bins), self.bin(state[2], self.pole_angle_bins), self.bin(state[3], self.pole_angular_velocity_bins))
 
 
-cart_position_bins = pd.cut([env.observation_space.low[0], env.observation_space.high[0]], bins=5, retbins=True)[1]
-cart_velocity_bins = pd.cut([env.observation_space.low[1], env.observation_space.high[1]], bins=5, retbins=True)[1]
-pole_angle_bins = pd.cut([env.observation_space.low[2], env.observation_space.high[2]], bins=10, retbins=True)[1]
-pole_angular_velocity_bins = pd.cut([env.observation_space.low[3], env.observation_space.high[3]], bins=10, retbins=True)[1]
+    def run(self):
+        q = np.zeros((5,5,10,10,2), dtype=float)
+        total_episode_rewards = []
+        for episode in range(self.episodes):
+            state = self.env.reset()
+            state = self.discretize(state)
+            done = False
+            episode_rewards = 0
 
+            while not done:
+                if random.random() < self.epsilon:
+                    action = self.env.action_space.sample()
+                else:
+                    action = np.argmax(q[state])
+                #PERFORM ACTION & COLLECT NEW STATE, REWARD, and DONE
+                next_state, reward, done, info = self.env.step(action)
 
-def bin(val, bins):
-    for i in range(len(bins)):
-        if val >= bins[i] and val < bins[i+1]:
-            return i
+                #ADJUST AND ROUND NEW STATE
+                next_state = self.discretize(next_state)
 
-def discretize(state):
-    return (bin(state[0], cart_position_bins), bin(state[1], cart_velocity_bins), bin(state[2], pole_angle_bins), bin(state[3], pole_angular_velocity_bins))
+                #UPDATE q
+                if not done:
+                    q[state][action] = q[state][action] + self.learning_rate * (reward + self.discount * np.amax(q[next_state]) - q[state][action])
+                else:
+                    q[state][action] = q[state][action] + self.learning_rate * (reward - q[state][action])
 
+                #ADD REWARD TO EPISODE_REWARDS
+                episode_rewards += reward
 
-if __name__ == "__main__":
-    q = np.zeros((5,5,10,10,2), dtype=float)
-    total_episode_rewards = []
-    epsilon = EPSILON
-    for episode in range(EPISODES):
-        state = env.reset()
-        state = discretize(state)
-        done = False
-        episode_rewards = 0
+                #Set state = next_state
+                state = next_state
 
-        while not done:
-            if random.random() < epsilon:
-                action = env.action_space.sample()
-            else:
-                action = np.argmax(q[state])
-            #PERFORM ACTION & COLLECT NEW STATE, REWARD, and DONE
-            next_state, reward, done, info = env.step(action)
+            #Decay Epsilon
+            self.epsilon = max(self.epsilon * self.epsilon_decay, 0.1)
+            
+            #ADD EPISODE REWARD TO TOTAL_EPISODE_REWARDS
+            total_episode_rewards.append(episode_rewards)
+            print("EPISODE: {}    REWARD: {}    EPSILON: {}".format(episode, episode_rewards, self.epsilon), end = '\r')
 
-            #ADJUST AND ROUND NEW STATE
-            next_state = discretize(next_state)
-
-            #UPDATE q
-            if not done:
-                q[state][action] = q[state][action] + LEARNING_RATE * (reward + DISCOUNT * np.amax(q[next_state]) - q[state][action])
-            else:
-                q[state][action] = q[state][action] + LEARNING_RATE * (reward - q[state][action])
-
-            #ADD REWARD TO EPISODE_REWARDS
-            episode_rewards += reward
-
-            #Set state = next_state
-            state = next_state
-
-        #Decay Epsilon
-        epsilon = max(epsilon * EPSILON_DECAY, 0.1)
-        
-        #ADD EPISODE REWARD TO TOTAL_EPISODE_REWARDS
-        total_episode_rewards.append(episode_rewards)
-        print("EPISODE: {}    REWARD: {}    EPSILON: {}".format(episode, episode_rewards, epsilon))
+if __name__ == '__main__':
+    Temporal_Difference().run()
